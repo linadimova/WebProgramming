13cb105500751e2cd926c3486088b84fadbb0fda
linadimova <163172089+linadimova@users.noreply.github.com>
2024-11-11 23:06:27 +0100

    Delete lab directory

--- lab/src/main/java/mk/ukim/finki/mk/lab/service/EventBookingService.java
+++
@@ -1,6 +0,0 @@
-package mk.ukim.finki.mk.lab.service;
-import mk.ukim.finki.mk.lab.model.EventBooking;
-
-public interface EventBookingService{
-    EventBooking placeBooking(String eventName, String attendeeName, String attendeeAddress, int numberOfTickets);
-}
--- lab/src/main/java/mk/ukim/finki/mk/lab/service/EventService.java
+++
@@ -1,8 +0,0 @@
-package mk.ukim.finki.mk.lab.service;
-import mk.ukim.finki.mk.lab.model.Event;
-import java.util.List;
-
-public interface EventService {
-    List<Event> listAll();
-    List<Event> searchEvents(String text);
-}
